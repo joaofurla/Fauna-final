2618b28551ddd0bdf1893262977ae5cb2568d489
joaofurla <jrfcarvalheiro@gmail.com>
2023-08-28 15:27:46 -0300

    .

---
+++ js/darkmode.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    darkModeToggle.addEventListener('change', function() {
+        document.body.classList.toggle('dark-mode');
+    });
+});
